cb7396bd69d675e422e828ac632308a7af6214b9
mia <miablidberg@gmail.com>
2026-09-04 15:00:02 +0200

    Add Supplier Service Model

---
+++ Assignment3.ConsoleApp/Suppliers/Services/SupplierService.cs
@@ -0,0 +1,16 @@
+﻿using Assignment3.ConsoleApp.Suppliers.Models;
+
+namespace Assignment3.ConsoleApp.Suppliers.Services;
+
+internal class SupplierService : ISupplierService
+{
+    public Supplier AddSupplier(Guid id, string supplierName, string supplierEmail, string? supplierPhoneNumer)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IReadOnlyList<Supplier> GetAllSuppliers()
+    {
+        throw new NotImplementedException();
+    }
+}
