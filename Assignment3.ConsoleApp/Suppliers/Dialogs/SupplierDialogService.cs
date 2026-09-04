9886f93defcfe8e984fa8f54bd065196982d8150
mia <miablidberg@gmail.com>
2026-09-04 15:19:41 +0200

    Add Supplier Dialog Service Model

---
+++ Assignment3.ConsoleApp/Suppliers/Dialogs/SupplierDialogService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment3.ConsoleApp.Suppliers.Dialogs
+{
+    internal class SupplierDialogService : ISupplierDialog
+    {
+        public void AddSupplierDialog()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowAllSuppliersDialog()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
