75825dceb1b65b6b55fe52dbf6b72f00ab0e7e5f
mia <miablidberg@gmail.com>
2026-09-04 14:11:53 +0200

    Add Supplier Model

---
+++ Assignment3.ConsoleApp/Suppliers/Models/Supplier.cs
@@ -0,0 +1,9 @@
+﻿namespace Assignment3.ConsoleApp.Suppliers.Models;
+
+public class Supplier(Guid id, string supplierName, string supplierEmail, string? supplierPhoneNumer)
+{
+    public Guid Id { get; init; } = id;
+    public string SupplierName { get; set; } = supplierName;
+    public string SupplierEmail { get; set; } = supplierEmail;
+    public string? SupplierPhoneNumber { get; set; } = supplierPhoneNumer;
+}
