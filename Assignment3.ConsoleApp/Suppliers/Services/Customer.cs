b50bbfc00080f952ea3ca34fb3a00fd9792a9751
mia <miablidberg@gmail.com>
2026-09-04 15:11:36 +0200

    Add a private list to store Suppliers in, a method to add a Supplier to the list and a private method to create a Supplier object

---
+++ Assignment3.ConsoleApp/Suppliers/Services/Customer.cs
@@ -0,0 +1,6 @@
+﻿namespace Assignment3.ConsoleApp.Suppliers.Services
+{
+    internal class Customer
+    {
+    }
+}
--- Assignment3.ConsoleApp/Suppliers/Services/SupplierService.cs
+++ Assignment3.ConsoleApp/Suppliers/Services/SupplierService.cs
@@ -4,9 +4,20 @@ namespace Assignment3.ConsoleApp.Suppliers.Services;
 
 internal class SupplierService : ISupplierService
 {
+    private readonly List<Supplier> _supplierList = [];
+
     public Supplier AddSupplier(Guid id, string supplierName, string supplierEmail, string? supplierPhoneNumer)
     {
-        throw new NotImplementedException();
+        Supplier supplier = CreateSupplier(supplierName, supplierEmail, supplierPhoneNumer);
+
+        _supplierList.Add(supplier);
+
+        return supplier;
+    }
+
+    private Supplier CreateSupplier(string supplierName, string supplierEmail, string? supplierPhoneNumer)
+    {
+        return new Supplier(Guid.NewGuid(), supplierName, supplierEmail, supplierPhoneNumer);
     }
 
     public IReadOnlyList<Supplier> GetAllSuppliers()
