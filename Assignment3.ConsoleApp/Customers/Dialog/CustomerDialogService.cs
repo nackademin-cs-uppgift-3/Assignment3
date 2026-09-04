d61855f00bb2a456f0a46f7d936327c0b5aadf31
lilyboyce96 <lily.esther.boyce@gmail.com>
2026-09-04 15:25:02 +0200

    Add Customer Dialog Service

---
+++ Assignment3.ConsoleApp/Customers/Dialog/CustomerDialogService.cs
@@ -0,0 +1,20 @@
+﻿using Assignment3.ConsoleApp.Customers.Services;
+
+namespace Assignment3.ConsoleApp.Customers.Dialog
+{
+    public class CustomerDialogService(ICustomerService customerService) : ICustomerDialog
+    {
+        public void AddCustomerDialog()
+        {
+            Console.WriteLine("Ange kundens namn:");
+            string name = Console.ReadLine() ?? "";
+            
+            Console.WriteLine("Ange kundens email:");
+            string email = Console.ReadLine() ?? "";
+
+            customerService.AddCustomer(name, email);
+
+            Console.WriteLine($"Kunden {name} med email {email} har lagts till.");
+        }
+    }
+}
